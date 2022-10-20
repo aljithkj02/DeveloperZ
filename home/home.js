6c8933b72ec00060695d861128921248f83d98e3
Aljith KJ <kwdeathlok@gmail.com>
2022-10-20 15:47:35 +0530

    Entertainment completed

--- home/home.js
+++ home/home.js
@@ -1,3 +1,5 @@
+// EARTH 
+
 let circle = document.querySelector(".circle");
 let earth = document.querySelector(".earth");
 let earthSection = document.querySelector("#earthSection");
@@ -15,3 +17,7 @@ nav.onmousemove = function (e) {
     circle.style.transform = "perspective(1000px) rotateY(" + x + "deg) rotateX(15deg) translateY(-30px)";
     earth.style.backgroundPositionX = y + "px";
 }
+
+// ENTERTAINMENT 
+
+
